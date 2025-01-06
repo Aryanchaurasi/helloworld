a5e59a944b3785ef6d1293e622844fa5ba8a1507
ARYANCHAURASIA <aryanchaurasi20@gmail.com>
2025-01-06 22:52:57 +0530

    This is a Array program

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array
+{
+	public static void main(String [] args)
+	{
+		int[] arr= new int[4];
+		arr[0]=20;
+		arr[1]=30;
+		arr[2]=40;
+		arr[3]=50;
+		System.out.println(arr[0]+""+arr[1]+""+arr[2]+""+arr[3]);
+	}
+}
